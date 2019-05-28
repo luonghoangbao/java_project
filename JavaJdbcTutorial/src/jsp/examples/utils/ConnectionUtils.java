03d6b9c2e8a90eb5e624a57264abe63cba6b9f76
baoluonghoang <baoluonghoang@gmail.com>
2019-05-28 11:10:46 -0700

    Performing Database Operations in Java

---
+++ JavaJdbcTutorial/src/jsp/examples/utils/ConnectionUtils.java
@@ -0,0 +1,57 @@
+package jsp.examples.utils;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class ConnectionUtils {
+ 
+  public static Connection getMyConnection() throws SQLException{
+	  String hostName = "localhost";
+	  String sqlInstanceName = "DESKTOP-9DMEFIP";
+	  String database = "manage_customer";
+	  String userName = "sa";
+	  String password = "1234";
+	  String connectionURL = "jdbc:jtds:sqlserver://" + hostName + ":1433/" + database + ";";
+	  Connection conn = DriverManager.getConnection(connectionURL, userName,
+	             password);
+      return conn;
+  }
+ 
+  public static void main(String[] args){
+	  Connection conn= null;
+	  try {
+		  conn = getMyConnection();
+		  String sql = "select * from news";
+		  Statement statement = conn.createStatement();
+		  ResultSet rs = statement.executeQuery(sql);
+		  System.out.println("Information of News :");
+		  while(rs.next()) {
+			  int id = rs.getInt("id");
+			  int category_id = rs.getInt("category_id");
+			  String description = rs.getString("description");
+			  String name = rs.getString("name");
+			  String detail = rs.getString("detail");
+			  String image = rs.getString("image"); 
+			  //Date date = (Date) rs.getDate("date");
+			  int user_id = rs.getInt("user_id");
+			  System.out.println("id:" + id + "   " + "category_id:" + category_id + "    " +  "name :" + name +   "     "  +   "description:"  + description +  "   " +  "detail:" + detail +  "   " +  "image:" + image + "   " /*+ "date:" + date.toString()*/ + "user_id:" + user_id);
+		  }
+		  String sql1 = "select name from categories";
+		  Statement statement1 = conn.createStatement();
+		  ResultSet rs1 = statement1.executeQuery(sql);
+		  System.out.println("Information of name from Categories:");
+		  while(rs1.next()) {
+			  String name = rs1.getString("name");
+			  System.out.println("name:" + name);
+		  }
+	  }catch(SQLException e) {
+		  e.printStackTrace();
+	  }
+	  System.out.println("connection successful: " + conn);
+  }
+}
+ 
